8a1c63ea1a0e347a926436de1f2ca804cf830fc6
Kuchinskas RR <hidanjkezor@gmail.com>
2021-05-14 22:05:25 +0300

    Init commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,43 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 Image.delete_all
 Image.reset_pk_sequence
 Image.create([
 
-               {name: 'Салями', file: 'pizza1.jpg', theme_id: 2},
-               {name: 'Маргарита', file: 'pizza2.jpg', theme_id: 3},
-               {name: 'Шпинат', file: 'pizza3.jpg', theme_id: 3},
-               {name: 'Ананас', file: 'pizza4.jpg', theme_id: 3},
-               {name: 'Говядина', file: 'pizza5.jpg', theme_id: 2},
-               {name: 'Сыр', file: 'pizza6.jpg', theme_id: 3},
-               {name: 'Карбонара', file: 'pizza7.jpg', theme_id: 2},
-               {name: 'Перец с сыром', file: 'pizza8.jpg', theme_id: 3},
-               {name: 'Пицца с курицой', file: 'pizza9.jpg', theme_id: 3},
-               {name: 'Карбонара с руколоолололо', file: 'pizza10.jpg', theme_id: 2},
-               {name: 'Пицца на томатной основе', file: 'pizza11.jpg', theme_id: 3},
-               {name: 'Сыр с томатом', file: 'pizza12.jpg', theme_id: 3},
-               {name: 'Солянка', file: 'pizza13.jpg', theme_id: 2},
-               {name: 'Кальцоне', file: 'pizza14.jpg', theme_id: 2},
-               {name: 'Тесто', file: 'pizza15.jpg', theme_id: 3},
-               {name: 'Томат', file: 'pizza16.jpg', theme_id: 3},
-               {name: 'Сыр с луком', file: 'pizza17.jpg', theme_id: 2},
-               {name: 'Вяленный томат', file: 'pizza18.jpg', theme_id: 3},
-               {name: 'Сыр с начинкой', file: 'pizza19.jpg', theme_id: 3},
-               {name: 'Студенческая', file: 'pizza20.jpg', theme_id: 2},
+               {name: 'Арагорн', file: 'Aragorn.jpg', theme_id: 2},
+               {name: 'Арагорн на тропах мёртвых', file: 'Aragorn_undead.jpg', theme_id: 2},
+               {name: 'Боромир', file: 'Boromir.jpg', theme_id: 2},
+               {name: 'Эомер', file: 'Eomer.jpg', theme_id: 2},
+               {name: 'Фродо с кольцом', file: 'frodo_with_ring.jpg', theme_id: 2},
+               {name: 'Галадриэль', file: 'Galadriel.jpg', theme_id: 2},
+               {name: 'Гимли', file: 'Gimli.jpg', theme_id: 2},
+               {name: 'Голум', file: 'Golum.jpg', theme_id: 3},
+               {name: 'Леголас', file: 'Legolas.jpg', theme_id: 2},
+               {name: 'Постер фильма: Властелин Колец: Возвращение короля', file: 'Lord_of_the_rings_poster.jpg', theme_id: 4},
+               {name: 'Мери и Пиппин', file: 'mery_and_pippinjpg.jpg', theme_id: 2},
+               {name: 'Назгул', file: 'nazgul_art.jpg', theme_id: 5},
+               {name: 'Саурон', file: 'Sauron_art.jpg', theme_id: 5},
+               {name: 'Саурон', file: 'Sauron_art2.jpg', theme_id: 5},
+               {name: 'Леголас', file: 'sauron_cosplay.jpg', theme_id: 6},
+               {name: 'Явление Саурона', file: 'sauron_eye.jpg', theme_id: 3},
+               {name: 'Постер к сериалу: Властелин Колец', file: 'the-lord-of-the-rings-poster.jpg', theme_id: 4},
+               {name: 'Саурон до своего падения', file: 'young_sauron.jpg', theme_id: 3},
              ])
 
 Theme.delete_all
 Theme.reset_pk_sequence
 Theme.create([
-               {name:"------"},# 1
-               {name: "Пицца Мясная"},  # 2
-               {name: "Пицца Веганская"},      # 3
+
+               {name: "Без категории"},        # 1 Нет темы
+               {name: "Добро"},        # 2
+               {name: "Зло"},          # 3
+               {name: "Постеры"},      # 4
+               {name: "Арты"},         # 5
+               {name: "Косплей"},      # 6
              ])
+
 User.delete_all
 User.reset_pk_sequence
 User.create([
 
-              {name: "Kate", email: "kate@mail.ru", password: "111", password_confirmation: "111"},
+              {name: "Example User", email: "example@railstutorial.org", password: "222222"},
             ])
